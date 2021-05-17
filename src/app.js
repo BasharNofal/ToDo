51290a684254d4ef6037d316a3ff2269df75c662
BasharNofal <bashar.nofal1997@gmail.com>
2021-05-18 01:19:28 +0300

    customs hooks left

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/header.js';
-import ToDo from './components/todo/todo.js';
+import ToDo from './components/todo/todo-connected';
 
 export default function App() {
 
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,19 +1,26 @@
 import React from 'react';
-
+import Toast from 'react-bootstrap/Toast';
+import ToastHeader from 'react-bootstrap/ToastHeader';
+import ToastBody from 'react-bootstrap/ToastBody';
 function ToDoList(props) {
 
   return (
     <ul>
       {props.list.map(item => (
-        <li
-          className={`complete-${item.complete.toString()}`}
+        <li className={`complete-${item.complete.toString()}`}
+
           key={item._id}>
-          <span onClick={() => props.handleComplete(item._id)}>
-            {item.text }
-            <p style={{textAlign:'right', margin:0}} >{`Assigned to: ${item.assignee}`}</p>
-          </span>
-          <button onClick={() => props.handleDelete(item._id)}>Delete</button>
-          <button onClick={() => props.handleUpdate(item._id)}>Edit</button>
+          <Toast>
+            <ToastHeader>
+              <span className={`rounded mr-2 mySpan`} onClick={() => props.handleComplete(item._id)} >{item.complete ? "Complete" : "Pending"}</span>
+              <span className="mr-auto" ><b>{item.assignee}</b></span>
+              <b className="closeBtn" onClick={() => props.handleDelete(item._id)}>X</b>
+            </ToastHeader>
+            <ToastBody>
+              <p> <b>{item.text}</b> </p>
+              <small>{`Difficulty: ${item.difficulty}`}</small>
+              </ToastBody>
+          </Toast>
         </li>
       ))}
     </ul>
